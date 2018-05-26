3f369df2822d25acce15b66d9bf2176432b87480
RikaFujimaki <mahimahi.kametan@gmail.com>
2018-05-27 00:06:32 +0900

    done

--- 1/home/step_first.cpp
+++ 1/home/step_first.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define FOR(i,a,b) for (int i=(a);i<(b);i++)
 #define rep(i,n) FOR(i,0,n)
 typedef long long ll;
-
+typedef pair<int, string> P;
 int main(){
 	//ファイルから読み込み
 	map<string, string> dict;
@@ -13,23 +13,64 @@ int main(){
 		cerr << "failed" << endl;
 		return -1;
 	}
-	string str1, str2;
-	while(getline(ifs, str1)){
-		getline(ifs, str2);
-		dict[str1] = str2;	//(aelpp, apple)
+	string fi, se;
+	while(getline(ifs, fi)){
+		getline(ifs, se);
+		dict[fi] = se;	//(aelpp, apple)
 	}
 
-	string a;
-	cin >> a;
-	sort(a.begin(), a.end());
-
+	string str2;
+	cin >> str2;
+	sort(str2.begin(), str2.end());
+/*
 	if (dict[a].size()!=0) {
 		cout << dict[a] << endl;
 	}
 	else {
 		cout << "NOT FOUND" << endl;
 	}
-	
+*/
+
+	//部分文字列ならok!
+	vector<P> ans;
+	vector<char> three{'j','k','q','x','z','J','K','Q','X','Z'};
+	vector<char> two{'c','f','h','l','m','p','v','w','y','C','F','H','L','M','P','V','W','Y'};
+	for (auto &item: dict) {
+		string str1 = item.first;
+		int i=0;
+		int len1 = str1.size();
+		int len2 = str2.size();	//16で決め打ちしてもよい？qu？
+		rep(j,len2){
+			if (str1[i]==str2[j]){
+				i++;
+			}
+		}
+		//問題点:quじゃなくて、uしか入ってないやつも...手動で排除？
+		if(i==len1){
+			//正解なら、点数をつけてansに入れる(あとでsortする)
+			int po = 0;
+			rep(k,len1){
+				if (find(three.begin(), three.end(), str1[k])!=three.end()){				
+					po += 3;
+				}
+				else if (find(two.begin(), two.end(), str1[k])!=two.end()){	
+					po += 2;
+				}
+				else{
+					po += 1;
+				}
+			}
+			ans.push_back(P(po, str1));
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+	int len = ans.size();
+	rep(k,len){
+		cout << ans[k].first << " " <<  dict[ans[k].second]<<endl;
+	}
+
+		
 	return 0;
 }
 
